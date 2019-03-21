9e793dd01acc861b6432b0d1f5868e7cb663d709
Arto Bendiken <arto@dryproject.org>
2019-03-22 00:30:11 +0200

    Made Length conform to Numeric.

--- Sources/DRYlib/qty/Length.swift
+++ Sources/DRYlib/qty/Length.swift
@@ -3,20 +3,74 @@
 /// A measure of distance.
 ///
 /// See: https://en.wikipedia.org/wiki/Length
-public struct Length<T>: Comparable, Equatable, Hashable
+public struct Length<T>: Comparable, Equatable, Hashable, Numeric
     where T: Comparable, T: Hashable, T: Numeric {
 
-  public let value: T
+  /// Conformance to Numeric
+  public typealias Magnitude = T.Magnitude
+  public typealias IntegerLiteralType = T.IntegerLiteralType
+
+  public var value: T
+
+  public init() {
+    self.value = 0
+  }
 
   public init(_ value: T) {
     self.value = value
   }
 
-  public static func <(lhs: Length<T>, rhs: Length<T>) -> Bool {
-    return lhs.value < rhs.value
+  /// Conformance to Numeric and ExpressibleByIntegerLiteral
+  public init(integerLiteral value: IntegerLiteralType) {
+    self.value = T(integerLiteral: value)
+  }
+
+  /// Conformance to Numeric
+  public init?<N>(exactly source: N) where N: BinaryInteger {
+    self.value = T(exactly: source)! // FIXME
   }
 
   public var isZero: Bool {
     return self.value == 0
+  }
+
+  /// Conformance to Numeric
+  public var magnitude: Length<T>.Magnitude {
+    return self.value.magnitude
+  }
+
+  /// Conformance to Comparable
+  public static func <(lhs: Length<T>, rhs: Length<T>) -> Bool {
+    return lhs.value < rhs.value
+  }
+
+  /// Conformance to Numeric
+  public static func +(lhs: Length<T>, rhs: Length<T>) -> Length<T> {
+    return Length<T>(lhs.value + rhs.value)
+  }
+
+  /// Conformance to Numeric
+  public static func -(lhs: Length<T>, rhs: Length<T>) -> Length<T> {
+    return Length<T>(lhs.value - rhs.value)
+  }
+
+  /// Conformance to Numeric
+  public static func *(lhs: Length<T>, rhs: Length<T>) -> Length<T> {
+    return Length<T>(lhs.value * rhs.value)
+  }
+
+  /// Conformance to Numeric
+  public static func +=(lhs: inout Length<T>, rhs: Length<T>) {
+    lhs.value += rhs.value
+  }
+
+  /// Conformance to Numeric
+  public static func -=(lhs: inout Length<T>, rhs: Length<T>) {
+    lhs.value -= rhs.value
+  }
+
+  /// Conformance to Numeric
+  public static func *=(lhs: inout Length<T>, rhs: Length<T>) {
+    lhs.value *= rhs.value
   }
 }
